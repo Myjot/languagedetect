3c0b44611f084a05fa24ae338bb5a25293ae4a6b
Marcin Jozefowicz <emjotek@gmail.com>
2018-04-09 13:04:59 +0200

    filled all files

---
+++ include/pojemnik.h
@@ -0,0 +1,17 @@
+#ifndef POJEMNIK_H
+#define POJEMNIK_H
+
+
+class pojemnik
+{
+    public:
+        pojemnik();
+        pojemnik(float tab[26]);
+        virtual ~pojemnik();
+        float frequencies[26];
+    protected:
+
+    private:
+};
+
+#endif // POJEMNIK_H
